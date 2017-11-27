78ee8c782b42136fd2fb905721c09399fedc0295
thorhop <thorhop@users.noreply.github.com>
2017-11-27 09:33:33 +0100

    Made the prompt functional
    Made the prompt functional again and added some options you can copy paste.  I also note that "muertos.als" is pretty much uselss without VSTs and samples. Good for parsing, though.

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ var prompt = require('prompt');
 
 prompt.start();
 
-console.log('Enter the path to the ALS file:');
+console.log('\nEnter the path to the ALS file:');
+console.log('Test options are:\n');
+console.log(' 1) test/als/MajorChords.als');
+console.log(' 2) test/als/muertos.als (Note: requires certain VSTs - will be replaced)\n');
 
+// A prompt that doesn't really work
 prompt.get('path', function (err, result) {
   //var ableton = new Ableton(result.path);
-  var ableton = new Ableton('test/als/muertos.als');
+  var ableton = new Ableton(result.path);
 
   ableton.read(function(error, $) {
     if (error) {
