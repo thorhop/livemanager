428740b3c133719a771b0f80a3a768b24e438bba
thorhop <thorhop@users.noreply.github.com>
2017-11-28 22:49:38 +0100

    Enter the parser...
    parser.js will contain the functions necessary to parse Ableton Live projects and will also include hooks to other node.js libraries to translate note names and hopefully some method to guess which scales are used in the project. This is not yet functional and I'm pushing it merely as a conversation piece.

--- parse.js
+++ parse.js
@@ -5,3 +5,39 @@
 // Here we will implement 'music-helper' to convert
 // numeric MIDI code in to keys, which will be combined
 // with note time to assert what chords are being played.
+
+var ableton = new Ableton(result.path);
+var Music = require('music-helper');
+
+function parseProject(project) {
+  ableton.read(function(error, $) {
+    if (error) {
+      console.error(error);
+    }
+    else {
+      var project = {
+        path = '',
+        pushScale = '',
+        rootNote = '',
+        tempo = [],
+        signature = [],
+        note = [],
+        notePlays = [],
+        guessedScales = []
+      };
+      project.rootNote = $('ScaleInformation RootNote').attr('value');
+      project.pushScale = $('ScaleInformation Name').attr('value');
+      // MidiClip
+      $('MidiClip').each(function(i){
+        $(this).find('KeyTrack').each(function(i){
+          project.note[i] = $(this).find('MidiKey').attr("value");
+          $(this).find('MidiNoteEvent').each(function(i, elem){
+            MidiNoteEvents[i] = $(this).attr('time');
+          });
+          project.notePlays[i] = MidiNoteEvents.join(', ');
+        });
+      })
+      console.log(project);
+    }
+  });
+}
